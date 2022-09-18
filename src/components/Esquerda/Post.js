e9ac5aad2ea86a5af9badf1722c56a36964672d6
duuda2011@gmail.com <elissonalvessilva@MacBook-Pro-de-Elisson.local>
2022-09-18 13:30:59 -0300

    criado o conteudo do componente post

--- src/components/Esquerda/Post.js
+++ src/components/Esquerda/Post.js
@@ -1,7 +1,84 @@
+import meowed from "../../assets/img/meowed.svg";
+import catcelphone from "../../assets/img/gato-telefone.svg";
+import respondeai from "../../assets/img/respondeai.svg";
+import barked from "../../assets/img/barked.svg";
+import dog from "../../assets/img/dog.svg";
+import adorableAnimals from "../../assets/img/adorable_animals.svg";
+
 export default function Post (){
   return(
-    <div className= "post">
-      
-    </div>
+    <div className="posts">
+          <div className="post">
+            <div className="topo">
+              <div className="usuario">
+                <img src= {meowed} alt="meowed"/>
+                meowed
+              </div>
+              <div className="acoes">
+                <ion-icon name="ellipsis-horizontal"></ion-icon>
+              </div>
+            </div>
+
+            <div className="conteudo">
+            <img src={catcelphone} alt="catcelphone"/>
+            </div>
+
+            <div className="fundo">
+              <div className="acoes">
+                <div>
+                  <ion-icon name="heart-outline"></ion-icon>
+                  <ion-icon name="chatbubble-outline"></ion-icon>
+                  <ion-icon name="paper-plane-outline"></ion-icon>
+                </div>
+                <div>
+                  <ion-icon name="bookmark-outline"></ion-icon>
+                </div>
+              </div>
+
+              <div className="curtidas">
+                <img src={respondeai} alt="respondeai"/>
+                <div className="texto">
+                  Curtido por <strong>respondeai</strong> e <strong>outras 101.523 pessoas</strong>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="post">
+            <div className="topo">
+              <div className="usuario">
+              <img src={barked} alt="barked"/>
+                barked
+              </div>
+              <div className="acoes">
+                <ion-icon name="ellipsis-horizontal"></ion-icon>
+              </div>
+            </div>
+
+            <div className="conteudo">
+              <img src={dog} alt="dog"/>
+            </div>
+
+            <div className="fundo">
+              <div className="acoes">
+                <div>
+                  <ion-icon name="heart-outline"></ion-icon>
+                  <ion-icon name="chatbubble-outline"></ion-icon>
+                  <ion-icon name="paper-plane-outline"></ion-icon>
+                </div>
+                <div>
+                  <ion-icon name="bookmark-outline"></ion-icon>
+                </div>
+              </div>
+
+              <div className="curtidas">
+              <img src={adorableAnimals} alt="adorableAnimals" />
+                <div className="texto">
+                  Curtido por <strong>adorable_animals</strong> e <strong>outras 99.159 pessoas</strong>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
   );
 }
