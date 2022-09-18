2b856441cd92d5eb69b223d10cd80ba14327e66a
duuda2011@gmail.com <elissonalvessilva@MacBook-Pro-de-Elisson.local>
2022-09-18 13:35:18 -0300

    criado o link e copyright do componente sidbar

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -5,6 +5,14 @@ export default function Sidebar (){
     <div className= "sidebar">
       <Usuario/>
       <Sugestoes/>
+       <div className="links">
+          Sobre • Ajuda • Imprensa • API • Carreiras • Privacidade • Termos • Localizações • Contas mais relevantes •
+          Hashtags • Idioma
+        </div>
+
+        <div className="copyright">
+          © 2021 INSTAGRAM DO FACEBOOK
+        </div>
     </div>
   );
 
