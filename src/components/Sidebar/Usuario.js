81f16eaedcdee41626cc4b1f3edc16a7b4b2b3dd
duuda2011@gmail.com <elissonalvessilva@MacBook-Pro-de-Elisson.local>
2022-09-19 16:48:49 -0300

    criado raciocinio para mudar a foto do usuario

--- src/components/Sidebar/Usuario.js
+++ src/components/Sidebar/Usuario.js
@@ -2,16 +2,22 @@ import { useState } from "react";
 import catanacomics from "../../assets/img/catanacomics.svg"
 export default function Usuario (){
   const [nomeUsuario, setNomeUsuario] = useState("catacomics")
+  const [caminhoImagem, setCaminhoImagem]=useState(catanacomics)
 
   function editarNome() {
     const promptEditarNome=prompt("deseja alterar o seu usuario?");
     setNomeUsuario(promptEditarNome);
   }
 
+ function editarImagem (){
+  const promptEditarImagem=prompt("novo link da imagem url")
+  setCaminhoImagem(promptEditarImagem);
+
+ }
   
   return(
      <div className="usuario">
-          <img src={catanacomics} alt="catanacomics"/>
+          <img src={caminhoImagem} alt="catanacomics" onClick={editarImagem}/>
           <div className="texto">
             <strong>{nomeUsuario}</strong>
             <span>
